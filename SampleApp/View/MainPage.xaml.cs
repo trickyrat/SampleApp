69ca967cdfe926e0cb28adbd0eef07bb70ba08a7
trickyrat <wangjian0513@outlook.com>
2023-04-20 01:02:48 +0800

    Add project files.

---
+++ SampleApp/View/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using SampleApp.ViewModel;
+
+namespace SampleApp.View;
+
+public partial class MainPage : ContentPage
+{
+	private MainViewModel vm = new MainViewModel();
+
+	public MainPage()
+	{
+        BindingContext = vm;
+        InitializeComponent();
+	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		vm.FullName = "Trickyrat";
+        vm.FavoriteFlower = "flower.jpg";
+    }
+}
+
---
+++ SampleApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SampleApp.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    //[ObservableProperty]
+    private string _fullName;
+
+    public string FullName
+    {
+        get { return _fullName; }
+        set
+        {
+            _fullName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _favoriteFlower;
+
+    public string FavoriteFlower
+    {
+        get { return _favoriteFlower; }
+        set
+        {
+            _favoriteFlower = value;
+            OnPropertyChanged();
+        }
+    }
+}
